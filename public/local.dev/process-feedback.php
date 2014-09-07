01502505b587022287e7f6d83eef0941ce7dde03
Owen Benjamin <owen@drunderlicence.com>
2014-09-07 21:41:46 +0100

    Tidy up db code

--- public/local.dev/process-feedback.php
+++ public/local.dev/process-feedback.php
@@ -1,59 +1,85 @@
 <?php
 
-    echo( "<h3>_POSTs</h3>" );
+    // CONSTANTS //
 
-    foreach ( $_POST as $key => $value )
-    {
-        echo $key . ' = ' . $value . '<br />';
-    }
-
-    echo( "<hr />" );
-
-    $feedbackType = $_POST['feedbackType'];
-    $feedbackDetails = $_POST['feedbackDetails'];
-    $feedbackResponse = ( $_POST['feedbackResponse'] == "on" ) ? "Response requested" : "Response not requested";
-
-    echo ( "<p>" . $feedbackType . "</p>" );
-    echo ( "<p>" . $feedbackDetails . "</p>" );
-    echo ( "<p>" . $feedbackResponse . "</p>" );
-
-    // Connect to mysql
     define ( "DB_USERNAME", "root" );
     define ( "DB_PASSWORD", "vagrant" );
+    define ( "DB_NAME", "solar_feedback" ); // FIXME or whatever we like
+    define ( "TABLE_NAME", "Feedback" );
+
+    define ( "VERBOSE_DEBUG", false );
+
+    function Bail( $error )
+    {
+        echo "<h3>Couldn't submit feedback</h3>";
+        if ( VERBOSE_DEBUG )
+        {
+            echo "<p>$error</p>";
+        }
+        else
+        {
+            echo "<p>Please contact XXX with details of the situtation</p>";
+        }
+        exit(1);
+    }
+
+
+    // Connect to mysql //
+
     $con = new mysqli( 'localhost', DB_USERNAME, DB_PASSWORD );
     if ( mysqli_connect_errno() )
     {
-        die ( "Couldn't connect to mysql: " . mysqli_connect_error() );
+        Bail( "Couldn't connect to mysql: " . mysqli_connect_error() );
+    }
+    else
+    {
+        if ( VERBOSE_DEBUG )
+        {
+            echo "<p>Connected to mysql...</p>";
+        }
     }
 
-    // Get db
-    define ( "DB_NAME", "solar_feedback" ); // FIXME or whatever
-    $db = $con->select_db( DB_NAME );
 
-    if ( !$db )
+    // Select or create db //
+
+    if ( !$con->select_db( DB_NAME ) )
     {
-        // Assume db doesn't exist and create it
+        if ( VERBOSE_DEBUG )
+        {
+            echo "<p>Creating db...</p>";
+        }
         $sql = 'CREATE DATABASE ' . DB_NAME;
-
         if ( $con->query( $sql ) )
         {
-            echo "Created db " . DB_NAME;
-            $db = $con->select_db( DB_NAME );
-            if ( !$db )
+            if ( !$con->select_db( DB_NAME ) ) // select newly created db
             {
-                die ( "Couldnt select db after creating?? " . $con->error );
+                Bail( "Couldn't select db after creating?? " . $con->error );
             }
         }
         else
         {
-            die ( "Couldn't create db: " . $con->error );
+            Bail( "Couldn't create db: " . $con->error );
         }
     }
 
-    // Create table if necessary
-    define ( "TABLE_NAME", "Feedback" );
-    if ( $con->query( "SELECT 1 FROM `" . TABLE_NAME . "`" ) == FALSE )
+    if ( VERBOSE_DEBUG )
     {
+        echo "<p>Selected db...</p>";
+    }
+
+    // Create table if necessary //
+
+    function TableExists( $con, $tableName )
+    {
+        return $con->query( "SELECT 1 FROM `$tableName`" );
+    }
+
+    if ( !TableExists( $con, TABLE_NAME ) )
+    {
+        if ( VERBOSE_DEBUG )
+        {
+            echo "<p>Creating table...</p>";
+        }
         $sql = "CREATE TABLE `" . TABLE_NAME . "`
             (
                 feedback_id INT NOT NULL AUTO_INCREMENT,
@@ -64,33 +90,41 @@
                 content TEXT,
                 response BOOL
             )";
-        if ( $con->query( $sql ) )
+        if ( !$con->query( $sql ) )
         {
-            echo ( "Made table" );
+            Bail( "Couldn't create table: " . $con->error );
         }
-        else
-        {
-            echo "Table query failed: " . $con->error;
-        }
-    }
-    else
-    {
-        echo ( "Did not make table" );
     }
 
-    // Insert data
+
+    // Insert data //
+
     $dbFeedbackType = $con->real_escape_string( $_POST[ 'feedbackType' ] );
     $dbFeedbackDetails = $con->real_escape_string( $_POST[ 'feedbackDetails' ] );
-    $dbFeedbackResponse = ( $_POST[ 'feedbackResponse' ] == "on" ) ? 1 : 0;
+    $dbFeedbackResponse = ( $_POST[ 'feedbackResponse' ] == $con->real_escape_string( "on" ) ) ? 1 : 0;
     $sql = "INSERT INTO `" . TABLE_NAME . "` (school, user, type, content, response)
         VALUES ('<SCHOOL>', '<USER>', '$dbFeedbackType', '$dbFeedbackDetails', '$dbFeedbackResponse')";
-    if ( $con->query( $sql ) )
+    if ( !$con->query( $sql ) )
     {
-        echo "added row";
+        Bail( "Insert row failed: " . $con->error );
     }
     else
     {
-        echo "Row failed: " . $con->error;
+        if ( VERBOSE_DEBUG )
+        {
+            echo "<p>Inserting row...</p>";
+        }
+    }
+
+
+    // Show confirmation //
+    // FIXME new page?
+
+    echo "<h3>Thank you for your feedback</h3>";
+
+    if ( $dbFeedbackResponse == 1 )
+    {
+        echo "<p>We will get back to you soon</p>";
     }
 
     $con->close();
