c2af65aff6382c1c22759b4682266f5648e2e972
Owen Benjamin <owen@drunderlicence.com>
2014-09-07 17:27:58 +0100

    Use dropdowns, proper text area, and checkbox for nicer ux

--- public/local.dev/process-feedback.php
+++ public/local.dev/process-feedback.php
@@ -1,8 +1,17 @@
 <?php
 
+    echo( "<h3>_POSTs</h3>" );
+
+    foreach ( $_POST as $key => $value )
+    {
+        echo $key . ' = ' . $value . '<br />';
+    }
+
+    echo( "<hr />" );
+
     $feedbackType = $_POST['feedbackType'];
     $feedbackDetails = $_POST['feedbackDetails'];
-    $feedbackResponse = ( $_POST['feedbackResponse'] == "yes" ) ? "Response requested" : "Response not requested";
+    $feedbackResponse = ( $_POST['feedbackResponse'] == "on" ) ? "Response requested" : "Response not requested";
 
     echo ( "<p>" . $feedbackType . "</p>" );
     echo ( "<p>" . $feedbackDetails . "</p>" );
