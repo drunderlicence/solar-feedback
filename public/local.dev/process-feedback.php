9f254ff13a7c89eb55c48fa8b4c48fe795a544c3
Owen Benjamin <owen@drunderlicence.com>
2014-09-12 17:15:31 +0100

    Became aware of php single vs double quotes and fixed the usage thereof

--- public/local.dev/process-feedback.php
+++ public/local.dev/process-feedback.php
@@ -2,16 +2,16 @@
 
     // CONSTANTS //
 
-    define ( "DB_USERNAME", "root" );
-    define ( "DB_PASSWORD", "vagrant" );
-    define ( "DB_NAME", "solar_feedback" ); // FIXME or whatever we like
-    define ( "TABLE_NAME", "Feedback" );
-    define ( "ADMIN_EMAIL", "test@test.com" ); // FIXME
-    define ( "VERBOSE_DEBUG", true );
+    define ( 'DB_USERNAME', 'root' );
+    define ( 'DB_PASSWORD', 'vagrant' );
+    define ( 'DB_NAME', 'solar_feedback' ); // FIXME or whatever we like
+    define ( 'TABLE_NAME', 'Feedback' );
+    define ( 'ADMIN_EMAIL', 'test@test.com' ); // FIXME
+    define ( 'VERBOSE_DEBUG', true );
 
     function Bail( $error )
     {
-        echo "<h3>Couldn't submit feedback</h3>";
+        echo '<h3>Couldn\'t submit feedback</h3>';
         if ( VERBOSE_DEBUG )
         {
             echo "<p>$error</p>";
@@ -30,13 +30,13 @@
     $con = new mysqli( 'localhost', DB_USERNAME, DB_PASSWORD );
     if ( mysqli_connect_errno() )
     {
-        Bail( "Couldn't connect to mysql: " . mysqli_connect_error() );
+        Bail( 'Couldn\'t connect to mysql: ' . mysqli_connect_error() );
     }
     else
     {
         if ( VERBOSE_DEBUG )
         {
-            echo "<p>Connected to mysql...</p>";
+            echo '<p>Connected to mysql...</p>';
         }
     }
 
@@ -47,25 +47,25 @@
     {
         if ( VERBOSE_DEBUG )
         {
-            echo "<p>Creating db...</p>";
+            echo '<p>Creating db...</p>';
         }
         $sql = 'CREATE DATABASE ' . DB_NAME;
         if ( $con->query( $sql ) )
         {
             if ( !$con->select_db( DB_NAME ) ) // select newly created db
             {
-                Bail( "Couldn't select db after creating?? " . $con->error );
+                Bail( 'Couldn\'t select db after creating?? ' . $con->error );
             }
         }
         else
         {
-            Bail( "Couldn't create db: " . $con->error );
+            Bail( 'Couldn\'t create db: ' . $con->error );
         }
     }
 
     if ( VERBOSE_DEBUG )
     {
-        echo "<p>Selected db...</p>";
+        echo '<p>Selected db...</p>';
     }
 
     // Create table if necessary //
@@ -79,9 +79,9 @@
     {
         if ( VERBOSE_DEBUG )
         {
-            echo "<p>Creating table...</p>";
+            echo '<p>Creating table...</p>';
         }
-        $sql = "CREATE TABLE `" . TABLE_NAME . "`
+        $sql = 'CREATE TABLE `' . TABLE_NAME . '`
             (
                 feedback_id INT NOT NULL AUTO_INCREMENT,
                 PRIMARY KEY(feedback_id),
@@ -90,32 +90,32 @@
                 type VARCHAR(15),
                 content TEXT,
                 response BOOL
-            )";
+            )';
         if ( !$con->query( $sql ) )
         {
-            Bail( "Couldn't create table: " . $con->error );
+            Bail( 'Couldn\'t create table: ' . $con->error );
         }
     }
 
 
     // Insert data //
 
-    $dbFeedbackSchool = "<SCHOOL>";
-    $dbFeedbackUser = "<USER>";
+    $dbFeedbackSchool = '<SCHOOL>';
+    $dbFeedbackUser = '<USER>';
     $dbFeedbackType = $con->real_escape_string( $_POST[ 'feedbackType' ] );
     $dbFeedbackDetails = $con->real_escape_string( $_POST[ 'feedbackDetails' ] );
-    $dbFeedbackResponse = ( $_POST[ 'feedbackResponse' ] == $con->real_escape_string( "on" ) ) ? 1 : 0;
+    $dbFeedbackResponse = ( $_POST[ 'feedbackResponse' ] == $con->real_escape_string( 'on' ) ) ? 1 : 0;
     $sql = "INSERT INTO `" . TABLE_NAME . "` (school, user, type, content, response)
         VALUES ('$dbFeedbackSchool', '$dbFeedbackUser', '$dbFeedbackType', '$dbFeedbackDetails', '$dbFeedbackResponse')";
     if ( !$con->query( $sql ) )
     {
-        Bail( "Insert row failed: " . $con->error );
+        Bail( 'Insert row failed: ' . $con->error );
     }
     else
     {
         if ( VERBOSE_DEBUG )
         {
-            echo "<p>Inserting row...</p>";
+            echo '<p>Inserting row...</p>';
         }
     }
 
@@ -123,8 +123,8 @@
     // Send email //
 
     $to = ADMIN_EMAIL;
-    $from = "test@test.com"; // FIXME something like this
-    $subject = "Solar Feedback Form";
+    $from = 'test@test.com'; // FIXME something like this
+    $subject = 'Solar Feedback Form';
 
     $fd = $_POST[ 'feedbackDetails' ]; // Grab directly from post preserves newlines FIXME - safe?
 
@@ -149,24 +149,24 @@ EEMMAAIILLSS;
 
     if ( !mail( $to, $subject, $message, $headers ) )
     {
-        Bail( "Couldn't send notification email." );
+        Bail( 'Couldn\'t send notification email.' );
     }
     else
     {
         if ( VERBOSE_DEBUG )
         {
-            echo "<p>Sending email...</p>";
+            echo '<p>Sending email...</p>';
         }
     }
 
     // Show confirmation //
     // FIXME new page?
 
-    echo "<h3>Thank you for your feedback</h3>";
+    echo '<h3>Thank you for your feedback</h3>';
 
     if ( $dbFeedbackResponse == 1 )
     {
-        echo "<p>We will get back to you soon</p>";
+        echo '<p>We will get back to you soon</p>';
     }
 
     $con->close();
